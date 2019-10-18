35adcf2d538435a17f5ee1cc72c4ef60bb4a55f6
Leon <leon0116cn@sina.com>
2019-10-18 14:16:51 +0800

    创建项目，优化了部分代码

--- ctrip/flight.py
+++ ctrip/flight.py
@@ -5,36 +5,50 @@ from datetime import datetime, timedelta
 
 
 USER_AGENT = [
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.75 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11',
-    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.163 Safari/535.1',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/77.0.3865.75 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/73.0.3683.103 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) '
+    'Chrome/17.0.963.56 Safari/535.11',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) '
+    'Chrome/14.0.835.163 Safari/535.1',
     'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0) Gecko/20100101 Firefox/6.0',
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:2.0.1) Gecko/20100101 Firefox/4.0.1',
-    'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
-    'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
+    'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) '
+    'Version/5.1 Safari/534.50',
+    'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) '
+    'Version/5.1 Safari/534.50',
     'Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) Presto/2.8.131 Version/11.11',
     'Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11',
     'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
     'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0',
-    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SE 2.X MetaSr 1.0; SE 2.X MetaSr 1.0; .NET CLR 2.0.50727; SE 2.X MetaSr 1.0)',
+    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SE 2.X MetaSr 1.0; SE 2.X MetaSr 1.0; '
+    '.NET CLR 2.0.50727; SE 2.X MetaSr 1.0)',
     'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; 360SE)',
     'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; TencentTraveler 4.0)'
 ]
 POST_HEADERS = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.75 Safari/537.36',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/77.0.3865.75 Safari/537.36',
     'referer': 'https://flights.ctrip.com/itinerary/oneway/sha-sia'
 }
 POST_TIMEOUT = 5
 POST_URL = 'https://flights.ctrip.com/itinerary/api/12808/products'
 
 
-def get_post_data(dcity='SHA', acity='SIA', days=0, flight_way='Oneway', class_type='ALL'):
+def get_post_data(dcity='SHA', acity='SIA', days=0, flight_way='Oneway', class_type='ALL', **kwargs):
     airport_params = []
     qdate = (datetime.now() + timedelta(days=days)).strftime('%Y-%m-%d')
     airport_params.append(dict(dcity=dcity, acity=acity, date=qdate))
-    post_data = {}
-    post_data = dict(flightWay=flight_way, classType=class_type, searchIndex=1, airportParams=airport_params, token='536bc24712ea27bb5109f78178533ba7')
+    post_data = dict(flightWay=flight_way, classType=class_type, searchIndex=1, airportParams=airport_params,
+                     token='536bc24712ea27bb5109f78178533ba7')
+    if kwargs and 'airline' in kwargs:
+        post_data['airline'] = kwargs['airline']
+    if kwargs and 'hasChild' in kwargs:
+        post_data['hasChild'] = kwargs['hasChild']
+    if kwargs and 'hasBaby' in kwargs:
+        post_data['hasBaby'] = kwargs['hasBaby']
     return post_data
 
 
@@ -43,12 +57,18 @@ def get_route_list(post_data):
         POST_HEADERS['user-agent'] = random.choice(USER_AGENT)
         r = requests.post(POST_URL, headers=POST_HEADERS, json=post_data, timeout=POST_TIMEOUT)
         r.raise_for_status()
+        show_request_detail(r)
         flight_data = r.json()
         if flight_data and flight_data.get('msg') == 'success':
             return flight_data.get('data').get('routeList')
     except HTTPError as e:
         print('{0} requests ERROR!'.format(POST_URL))
         print(e)
+
+
+def show_request_detail(response):
+    print('Request url is : {0}'.format(response.url))
+    print('Request body is {0}:'.format(response.request.body))
 
 
 def parser_route_list(route_list, route_data):
@@ -106,7 +126,10 @@ def parser_route_list(route_list, route_data):
             departure_terminal_id = departure_airport_info.get('terminal').get('id')
             departure_terminal_short_name = departure_airport_info.get('terminal').get('shortName')
 
-            route_data.append((airline_name, flight_number, lowest_price, departure_date, departure_airport_name, departure_terminal_name, arrival_date, arrival_airport_name, arrival_terminal_name, punctuality_rate, shared_flight_name, shared_flight_number, craft_type_name, craft_type_kind_display_name, meal_flag))
+            route_data.append((airline_name, flight_number, lowest_price, departure_date, departure_airport_name,
+                               departure_terminal_name, arrival_date, arrival_airport_name, arrival_terminal_name,
+                               punctuality_rate, shared_flight_name, shared_flight_number, craft_type_name,
+                               craft_type_kind_display_name, meal_flag))
     return route_data
 
 
--- ctrip/poi.py
+++ ctrip/poi.py
@@ -3,70 +3,92 @@ import requests
 from requests.exceptions import HTTPError
 
 
-CTRIP_POI_URL = "https://flights.ctrip.com/itinerary/api/poi/get"
-REQUESTS_TIMEOUT = 10
+REQUEST_TIMEOUT = 5
 USER_AGENT = [
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.75 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11',
-    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.163 Safari/535.1',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/77.0.3865.75 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/73.0.3683.103 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) '
+    'Chrome/17.0.963.56 Safari/535.11',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) '
+    'Chrome/14.0.835.163 Safari/535.1',
     'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0) Gecko/20100101 Firefox/6.0',
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:2.0.1) Gecko/20100101 Firefox/4.0.1',
-    'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
-    'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
+    'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) '
+    'Version/5.1 Safari/534.50',
+    'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) '
+    'Version/5.1 Safari/534.50',
     'Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) Presto/2.8.131 Version/11.11',
     'Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11',
     'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
     'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0',
-    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SE 2.X MetaSr 1.0; SE 2.X MetaSr 1.0; .NET CLR 2.0.50727; SE 2.X MetaSr 1.0)',
+    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SE 2.X MetaSr 1.0; SE 2.X MetaSr 1.0; '
+    '.NET CLR 2.0.50727; SE 2.X MetaSr 1.0)',
     'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; 360SE)',
     'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; TencentTraveler 4.0)'
 ]
-POST_HEADERS = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.75 Safari/537.36',
-}
-POI_LABEL = ['pn', 'fn', 'sn', 'snc', 'cate', 'cn']
+CITY_POI_LABEL = ['pn', 'fn', 'sn', 'snc', 'cate', 'cn']
 
 
-def get_flight_poi():
+def do_request_get(url):
     try:
-        POST_HEADERS['user-agent'] = random.choice(USER_AGENT)
-        r = requests.get(CTRIP_POI_URL, headers=POST_HEADERS, timeout=REQUESTS_TIMEOUT)
+        headers = {'user-agent': random.choice(USER_AGENT)}
+        r = requests.get(url, headers=headers, timeout=REQUEST_TIMEOUT)
         r.raise_for_status()
-        poi_json_data = r.json()
-        if poi_json_data and poi_json_data.get('msg') == 'success':
-            return poi_json_data.get('data')
+        data = r.json()
+        if data and data.get('msg') == 'success':
+            return data
     except HTTPError as e:
-        print('{0} requests ERROR!'.format(CTRIP_POI_URL))
+        print('{0} requests error!'.format(url))
         print(e)
 
 
-def parser_flight_poi(json_data, poi_data):
-    for key1, item_first in json_data.items():
+def parser_city_poi(json_data):
+    poi_data = []
+    for key1, item_first in json_data.get('data').items():
         if isinstance(item_first, dict):
             for key2, item_second in item_first.items():
                 for item in item_second:
                     poi_item = item.get('data').split('|')
                     poi_item.append(key1)
                     poi_item.append(item.get('display'))
-                    poi_data.append(dict(zip(POI_LABEL, poi_item)))
+                    poi_data.append(poi_item)
         elif isinstance(item_first, list):
             for item in item_first:
                 poi_item = item.get('data').split('|')
                 poi_item.append(key1)
                 poi_item.append(item.get('display'))
-                poi_data.append(dict(zip(POI_LABEL, poi_item)))
+                poi_data.append(poi_item)
+    return poi_data
 
 
-def print_flight_poi(poi_data):
+def parser_country_code(json_data):
+    poi_data = []
+    for item in json_data.get('data'):
+        poi_data.append(list(item.values()))
+    return poi_data
+
+
+def get_city_poi():
+    url = "https://flights.ctrip.com/itinerary/api/poi/get"
+    poi_json = do_request_get(url)
+    poi_data = parser_city_poi(poi_json)
     print(poi_data)
+    return poi_data
+
+
+def get_country_code():
+    url = "https://flights.ctrip.com/international/search/api/country/getCountryCodes?v=0.9785196447487685"
+    poi_json = do_request_get(url)
+    poi_data = parser_country_code(poi_json)
+    print(poi_data)
+    return poi_data
 
 
 def main():
-    poi_data = []
-    json_data = get_flight_poi()
-    parser_flight_poi(json_data, poi_data)
-    print_flight_poi(poi_data)
+#    get_city_poi()
+    get_country_code()
 
 
 if __name__ == '__main__':
