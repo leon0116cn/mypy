dc0e3d56986a2c26bce874e6dfaacf1a12a4d390
Leon <leon0116cn@sina.com>
2019-10-24 17:47:48 +0800

    优化了国内机票爬取功能

--- ctrip/domesticflight.py
+++ ctrip/domesticflight.py
@@ -3,7 +3,6 @@ import requests
 from requests.exceptions import HTTPError
 from datetime import datetime, timedelta
 
-
 USER_AGENT = [
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
     'Chrome/77.0.3865.75 Safari/537.36',
@@ -28,62 +27,61 @@ USER_AGENT = [
     'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; 360SE)',
     'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; TencentTraveler 4.0)'
 ]
-POST_HEADERS = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                  'Chrome/77.0.3865.75 Safari/537.36',
-    'referer': 'https://flights.ctrip.com/itinerary/oneway/sha-sia'
-}
-POST_TIMEOUT = 5
-POST_URL = 'https://flights.ctrip.com/itinerary/api/12808/products'
+DOMESTIC_FLIGHT_SEARCH_URL = "https://flights.ctrip.com/itinerary/api/12808/products"
+DOMESTIC_FLIGHT_SEARCH_REFERER = "https://flights.ctrip.com/itinerary/oneway/sha-sia"
+REQUEST_TIMEOUT = 5
 
 
-def get_post_data(dcity='SHA', acity='SIA', days=0, flight_way='Oneway', class_type='ALL', **kwargs):
+def get_domestic_flight_search_criteria(dcity='SHA', acity='SIA', days=20, **kwargs):
     airport_params = []
     qdate = (datetime.now() + timedelta(days=days)).strftime('%Y-%m-%d')
-    airport_params.append(dict(dcity=dcity, acity=acity, date=qdate))
-    post_data = dict(flightWay=flight_way, classType=class_type, searchIndex=1, airportParams=airport_params,
-                     token='536bc24712ea27bb5109f78178533ba7')
-    if kwargs and 'airline' in kwargs:
-        post_data['airline'] = kwargs['airline']
-    if kwargs and 'hasChild' in kwargs:
-        post_data['hasChild'] = kwargs['hasChild']
-    if kwargs and 'hasBaby' in kwargs:
-        post_data['hasBaby'] = kwargs['hasBaby']
-    return post_data
+    airport_params.append({'dcity': dcity, 'acity': acity, 'date': qdate})
+    search_criteria = {
+        'flightWay': 'Oneway',
+        'classType': 'ALL',
+        'searchIndex': 1,
+        'airportParams': airport_params,
+        'token': '536bc24712ea27bb5109f78178533ba7'
+    }
+    if 'airline' in kwargs:
+        search_criteria['airline'] = kwargs['airline']
+    if 'hasChild' in kwargs:
+        search_criteria['hasChild'] = kwargs['hasChild']
+    if 'hasBaby' in kwargs:
+        search_criteria['hasBaby'] = kwargs['hasBaby']
+    return search_criteria
 
 
-def get_route_list(post_data):
-    try:
-        POST_HEADERS['user-agent'] = random.choice(USER_AGENT)
-        r = requests.post(POST_URL, headers=POST_HEADERS, json=post_data, timeout=POST_TIMEOUT)
-        r.raise_for_status()
-        show_request_detail(r)
-        flight_data = r.json()
-        if flight_data and flight_data.get('msg') == 'success':
-            return flight_data.get('data').get('routeList')
-    except HTTPError as e:
-        print('{0} requests ERROR!'.format(POST_URL))
-        print(e)
+def request_domestic_flight_search_criteria(search_criteria):
+    if search_criteria:
+        headers = {'user-agent': random.choice(USER_AGENT), 'referer': DOMESTIC_FLIGHT_SEARCH_REFERER}
+        try:
+            r = requests.post(DOMESTIC_FLIGHT_SEARCH_URL, headers=headers, json=search_criteria,
+                              timeout=REQUEST_TIMEOUT)
+            r.raise_for_status()
+            show_request_params_detail(r)
+            data = r.json()
+            if data and data.get('msg') == 'success':
+                return data
+        except HTTPError as e:
+            print('{0} requests error!'.format(r.url))
+            print(e)
 
 
-def show_request_detail(response):
-    print('Request url is : {0}'.format(response.url))
-    print('Request body is {0}:'.format(response.request.body))
-
-
-def parser_route_list(route_list, route_data):
-    for route in route_list:
+def parser_domestic_flight_json(json_data):
+    flight_data = []
+    for route in json_data.get('data').get('routeList'):
         if route.get('routeType') == 'Flight':
             leg = route.get('legs')[0]
             flight_id = leg.get('flightId')
             leg_type = leg.get('legType')
 
-            #航班价格信息
+            # 航班价格信息
             characteristic = leg.get('characteristic')
             lowest_price = characteristic.get('lowestPrice')
             lowest_child_price = characteristic.get('lowestChildPrice')
 
-            #航班信息
+            # 航班信息
             flight = leg.get('flight')
             airline_code = flight.get('airlineCode')
             airline_name = flight.get('airlineName')
@@ -106,7 +104,7 @@ def parser_route_list(route_list, route_data):
             else:
                 shared_flight = False
 
-            #到达机场信息
+            # 到达机场信息
             arrival_airport_info = leg.get('flight').get('arrivalAirportInfo')
             arrival_airport_name = arrival_airport_info.get('airportName')
             arrival_airport_tlc = arrival_airport_info.get('airportTlc')
@@ -116,7 +114,7 @@ def parser_route_list(route_list, route_data):
             arrival_terminal_id = arrival_airport_info.get('terminal').get('id')
             arrival_terminal_short_name = arrival_airport_info.get('terminal').get('shortName')
 
-            #起飞机场信息
+            # 起飞机场信息
             departure_airport_info = leg.get('flight').get('departureAirportInfo')
             departure_airport_name = departure_airport_info.get('airportName')
             departure_airport_tlc = departure_airport_info.get('airportTlc')
@@ -126,24 +124,40 @@ def parser_route_list(route_list, route_data):
             departure_terminal_id = departure_airport_info.get('terminal').get('id')
             departure_terminal_short_name = departure_airport_info.get('terminal').get('shortName')
 
-            route_data.append((airline_name, flight_number, lowest_price, departure_date, departure_airport_name,
-                               departure_terminal_name, arrival_date, arrival_airport_name, arrival_terminal_name,
-                               punctuality_rate, shared_flight_name, shared_flight_number, craft_type_name,
-                               craft_type_kind_display_name, meal_flag))
-    return route_data
+            flight_data.append((airline_name, flight_number, lowest_price, departure_date, departure_airport_name,
+                                departure_terminal_name, arrival_date, arrival_airport_name, arrival_terminal_name,
+                                punctuality_rate, shared_flight_name, shared_flight_number, craft_type_name,
+                                craft_type_kind_display_name, meal_flag))
+    return flight_data
 
 
-def print_route_data(route_data):
-    for route in route_data:
-        print(route)
+def get_domestic_flight_data():
+    search_criteria = get_domestic_flight_search_criteria()
+    json_data = request_domestic_flight_search_criteria(search_criteria)
+    flight_data = parser_domestic_flight_json(json_data)
+    return flight_data
+
+
+def show_request_params_detail(r):
+    if r:
+        method = r.request.method
+        if method == 'GET':
+            print('*********' * 5)
+            print('Request method is : {0}'.format(method))
+            print('Request url is : {0}'.format(r.url))
+            print('Request headers is {0}'.format(r.request.headers))
+            print('*********' * 5)
+        if method == 'POST':
+            print('*********' * 5)
+            print('Request method is : {0}'.format(method))
+            print('Request url is : {0}'.format(r.url))
+            print('Request body is {0}:'.format(r.request.body))
+            print('Request headers is {0}'.format(r.request.headers))
+            print('*********' * 5)
 
 
 def main():
-    post_data = get_post_data(days=2)
-    route_list = get_route_list(post_data)
-    route_data = []
-    parser_route_list(route_list, route_data)
-    print_route_data(route_data)
+    print(get_domestic_flight_data())
 
 
 if __name__ == '__main__':
