eafc3a056aaa5d7ce675219141900da865fd1def
Leon <leon0116cn@sina.com>
2019-10-28 16:14:56 +0800

    优化了国内机票json数据解析代码

---
+++ ctrip/settings.py
@@ -0,0 +1,33 @@
+USER_AGENT = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/77.0.3865.75 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/73.0.3683.103 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) '
+    'Chrome/17.0.963.56 Safari/535.11',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) '
+    'Chrome/14.0.835.163 Safari/535.1',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0) Gecko/20100101 Firefox/6.0',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:2.0.1) Gecko/20100101 Firefox/4.0.1',
+    'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) '
+    'Version/5.1 Safari/534.50',
+    'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) '
+    'Version/5.1 Safari/534.50',
+    'Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) Presto/2.8.131 Version/11.11',
+    'Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0',
+    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SE 2.X MetaSr 1.0; SE 2.X MetaSr 1.0; '
+    '.NET CLR 2.0.50727; SE 2.X MetaSr 1.0)',
+    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; 360SE)',
+    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; TencentTraveler 4.0)'
+]
+REQUEST_TIMEOUT = 5
+DATAFRAME_COLUMNS = ['flightNumber', 'airlineName', 'airlineCode', 'sharedFlightNumber',
+                     'sharedFlightName', 'departureDate', 'arrivalDate', 'punctualityRate',
+                     'departure-cityName', 'departure-cityTlc', 'departure-airportName', 'departure-airportTlc',
+                     'departure-terminal-name', 'arrival-cityName', 'arrival-cityTlc', 'arrival-airportName',
+                     'arrival-airportTlc', 'arrival-terminal-name', 'craftTypeName', 'craftKind', 'oilFee', 'tax',
+                     'lowestPrice', 'lowestChildPrice', 'lowestChildAdultPrice', 'lowestCfPrice', 'lowestChildCfPrice',
+                     'lowestChildAdultCfPrice', 'Y-cabinClass', 'C-cabinClass', 'F-cabinClass', 'mealFlag', 'mealType']
+EXPORT_FILE_PATH = 'E:\\MyProjects\\Pycharm\\mypy\\data\\'
