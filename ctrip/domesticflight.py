d0028977a57a94783fce3cd28ffcb70c8ee99a09
Leon <leon0116cn@sina.com>
2019-10-28 16:14:31 +0800

    优化了国内机票json数据解析代码

--- ctrip/domesticflight.py
+++ ctrip/domesticflight.py
@@ -1,36 +1,15 @@
 import random
+from datetime import datetime, timedelta
+import os
 import requests
 from requests.exceptions import HTTPError
-from datetime import datetime, timedelta
+import pandas as pd
+from ctrip.settings import USER_AGENT, REQUEST_TIMEOUT, DATAFRAME_COLUMNS, EXPORT_FILE_PATH
 
-USER_AGENT = [
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Chrome/77.0.3865.75 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Chrome/73.0.3683.103 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) '
-    'Chrome/17.0.963.56 Safari/535.11',
-    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) '
-    'Chrome/14.0.835.163 Safari/535.1',
-    'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0) Gecko/20100101 Firefox/6.0',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:2.0.1) Gecko/20100101 Firefox/4.0.1',
-    'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) '
-    'Version/5.1 Safari/534.50',
-    'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) '
-    'Version/5.1 Safari/534.50',
-    'Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) Presto/2.8.131 Version/11.11',
-    'Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11',
-    'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
-    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0',
-    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SE 2.X MetaSr 1.0; SE 2.X MetaSr 1.0; '
-    '.NET CLR 2.0.50727; SE 2.X MetaSr 1.0)',
-    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; 360SE)',
-    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; TencentTraveler 4.0)'
-]
+
 DOMESTIC_FLIGHT_SEARCH_URL = "https://flights.ctrip.com/itinerary/api/12808/products"
 DOMESTIC_FLIGHT_SEARCH_REFERER = "https://flights.ctrip.com/itinerary/oneway/sha-sia"
 DOMESTIC_FLIGHT_SEARCH_TOKEN = '536bc24712ea27bb5109f78178533ba7'
-REQUEST_TIMEOUT = 5
 
 
 def get_domestic_flight_search_criteria(dcity='SHA', acity='SIA', days=20, **kwargs):
@@ -69,73 +48,57 @@ def request_domestic_flight_search_criteria(search_criteria):
             print(e)
 
 
-def parser_domestic_flight_json(json_data):
+def parser_domestic_flight(json_data):
     flight_data = []
     for route in json_data.get('data').get('routeList'):
         if route.get('routeType') == 'Flight':
-            leg = route.get('legs')[0]
-            flight_id = leg.get('flightId')
-            leg_type = leg.get('legType')
+            flight_item = dict(routeType=route.get('routeType'))
+            #航班信息
+            flight = route.get('legs')[0].get('flight')
+            departureAirportInfo = parser_airport_info(flight.pop('departureAirportInfo'), 'departure')
+            if departureAirportInfo:
+                flight.update(departureAirportInfo)
+            arrivalAirportInfo = parser_airport_info(flight.pop('arrivalAirportInfo'), 'arrival')
+            if arrivalAirportInfo:
+                flight.update(arrivalAirportInfo)
+            flight_item.update(flight)
+            #机票价格信息
+            characteristic = route.get('legs')[0].get('characteristic')
+            standardPrices = parser_standard_prices(characteristic.pop('standardPrices'))
+            if standardPrices:
+                characteristic.update(standardPrices)
+            flight_item.update(characteristic)
 
-            # 航班价格信息
-            characteristic = leg.get('characteristic')
-            lowest_price = characteristic.get('lowestPrice')
-            lowest_child_price = characteristic.get('lowestChildPrice')
-
-            # 航班信息
-            flight = leg.get('flight')
-            airline_code = flight.get('airlineCode')
-            airline_name = flight.get('airlineName')
-            flight_number = flight.get('flightNumber')
-            departure_date = flight.get('departureDate')
-            arrival_date = flight.get('arrivalDate')
-            punctuality_rate = flight.get('punctualityRate')
-            craft_kind = flight.get('craftKind')
-            craft_type_code = flight.get('craftTypeCode')
-            craft_type_kind_display_name = flight.get('craftTypeKindDisplayName')
-            craft_type_name = flight.get('craftTypeName')
-            duration_days = flight.get('durationDays')
-            fid = flight.get('id')
-            meal_flag = flight.get('mealFlag')
-            meal_type = flight.get('mealType')
-            shared_flight_name = flight.get('sharedFlightName')
-            shared_flight_number = flight.get('sharedFlightNumber')
-            if shared_flight_name is not None:
-                shared_flight = True
-            else:
-                shared_flight = False
-
-            # 到达机场信息
-            arrival_airport_info = leg.get('flight').get('arrivalAirportInfo')
-            arrival_airport_name = arrival_airport_info.get('airportName')
-            arrival_airport_tlc = arrival_airport_info.get('airportTlc')
-            arrival_city_name = arrival_airport_info.get('cityName')
-            arrival_city_tlc = arrival_airport_info.get('cityTlc')
-            arrival_terminal_name = arrival_airport_info.get('terminal').get('name')
-            arrival_terminal_id = arrival_airport_info.get('terminal').get('id')
-            arrival_terminal_short_name = arrival_airport_info.get('terminal').get('shortName')
-
-            # 起飞机场信息
-            departure_airport_info = leg.get('flight').get('departureAirportInfo')
-            departure_airport_name = departure_airport_info.get('airportName')
-            departure_airport_tlc = departure_airport_info.get('airportTlc')
-            departure_city_name = departure_airport_info.get('cityName')
-            departure_city_tlc = departure_airport_info.get('cityTlc')
-            departure_terminal_name = departure_airport_info.get('terminal').get('name')
-            departure_terminal_id = departure_airport_info.get('terminal').get('id')
-            departure_terminal_short_name = departure_airport_info.get('terminal').get('shortName')
-
-            flight_data.append((airline_name, flight_number, lowest_price, departure_date, departure_airport_name,
-                                departure_terminal_name, arrival_date, arrival_airport_name, arrival_terminal_name,
-                                punctuality_rate, shared_flight_name, shared_flight_number, craft_type_name,
-                                craft_type_kind_display_name, meal_flag))
+            flight_data.append(flight_item)
     return flight_data
+
+
+def parser_airport_info(airport, flag):
+    if airport:
+        flaged_airport = {}
+        for key, value in airport.items():
+            if isinstance(value, dict):
+                terminal = airport.get(key)
+                for k, v in terminal.items():
+                    flaged_airport['-'.join((flag, key, k))] = v
+            else:
+                flaged_airport['-'.join((flag, key))] = value
+        return flaged_airport
+
+
+def parser_standard_prices(standard_prices):
+    prices = {}
+    if standard_prices:
+        for price in standard_prices:
+            key = '-'.join((price['cabinClass'], 'cabinClass'))
+            prices[key] = price['price']
+        return prices
 
 
 def get_domestic_flight_data():
     search_criteria = get_domestic_flight_search_criteria()
     json_data = request_domestic_flight_search_criteria(search_criteria)
-    flight_data = parser_domestic_flight_json(json_data)
+    flight_data = parser_domestic_flight(json_data)
     return flight_data
 
 
@@ -157,8 +120,16 @@ def show_request_params_detail(r):
             print('*********' * 5)
 
 
+def export_domestic_flight(flight_data):
+    df = pd.DataFrame(flight_data, columns=DATAFRAME_COLUMNS)
+    now = datetime.now().strftime('%Y-%m-%d')
+    file_name = os.path.join(EXPORT_FILE_PATH, '{0}.xlsx'.format(now))
+    df.to_excel(file_name)
+
+
 def main():
-    print(get_domestic_flight_data())
+    flight_data = get_domestic_flight_data()
+    export_domestic_flight(flight_data)
 
 
 if __name__ == '__main__':
